4b9c4100cb54043f28e3c807c01ef214ffad86a5
DeRpYsApLiNg <itman1159@hotmail.com>
2016-02-29 16:43:09 -0700

    Update extension.js

--- extension.js
+++ extension.js
@@ -38,6 +38,35 @@
               }
             };
 
+        bot.commands.websiteCommand = {
+            command: 'website',  // The command to be called. With the standard command literal this would be: !bacon
+            rank: 'user', // Minimum user permission to use the command
+            type: 'exact', // Specify if it can accept variables or not (if so, these have to be handled yourself through the chat.message
+              functionality: function (chat, cmd) {
+                if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+                if (!bot.commands.executable(this.rank, chat)) return void (0);
+                else {
+                  API.sendChat("Our website is located at http://bit.ly/1LrzESj");
+                }
+              }
+            };
+            
+                    bot.commands.twitterCommand = {
+            command: 'website',  // The command to be called. With the standard command literal this would be: !bacon
+            rank: 'user', // Minimum user permission to use the command
+            type: 'exact', // Specify if it can accept variables or not (if so, these have to be handled yourself through the chat.message
+              functionality: function (chat, cmd) {
+                if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+                if (!bot.commands.executable(this.rank, chat)) return void (0);
+                else {
+                  API.sendChat("Our twitter is located at http://bit.ly/1pmeOtQ");
+                }
+              }
+            };
+            
+            
+      
+            
         // Load the chat package again to account for any changes
         bot.loadChat();
 
